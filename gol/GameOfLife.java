988473180585b14d60b32b398ef6e641a4cf137a
alexander chang <alexchangsports@gmail.com>
2025-12-13 02:41:34 +0000

    Test run

--- gol/GameOfLife.java
+++ gol/GameOfLife.java
@@ -10,6 +10,7 @@ public class GameOfLife implements Board {
     public GameOfLife(int x, int y)
     {
         // Construct a 2d array of the given x and y size.
+        board = new int[x][y];
     }
 
     // Set values on the board
@@ -24,60 +25,9 @@ public class GameOfLife implements Board {
     // Run the simulation for a number of turns
     public void run(int turns) {
         // call step the number of times requested
-    }
-
-    // Step the simulation forward one turn.
-    public void step()
-    {
-        print();
-        // Update the game board, store a 1 if the cell is alive and a 0 otherwise.
-    }
-
-
-    public int countNeighbors(int x, int y) {
-        int count = 0;
-        // count the number of neighbors the cell has
-        // use the get(x,y) method to read any board state you need.
-        return count;
-    }
-
-    // Get a value from the board with "wrap around"
-    // Locations outside the board will loop back into the board.
-    // Ex: -1 will read board.length-1
-    public int get(int x, int y) {
-        int xLimit = board.length;
-        int yLimit= board[0].length;
-        return board[(x+xLimit)%xLimit][(y+yLimit)%yLimit];
-    }
-
-    // Test helper to get the whole board state
-    public int[][] get()
-    {
-        return board;
-    }
-
-    // Test helper to print the current state
-    public void print(){
-        // Print the header
-        System.out.print("\n ");
-        for (int y = 0; y < board[0].length; y++) {
-            System.out.print(y%10 + " ");
+        for (int i = 0; i < turns; i++) {
+            step();
         }
-
-        for (int x = 0; x < board.length; x++) {
-            System.out.print("\n" + x%10);
-            for (int y=0; y<board[x].length; y++)
-            {
-                if (board[x][y] == 1)
-                {
-                    System.out.print("⬛");
-                }
-                else
-                {
-                    System.out.print("⬜");
-                }
-            }
-        }
-        System.out.println();
     }
-}
+
+   
