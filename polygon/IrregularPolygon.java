ddadc3c1b2a2259ca14af4283579a5f6a7cc762d
alexander chang <alexchangsports@gmail.com>
2026-03-05 21:45:02 +0000

    edit/set up

--- polygon/IrregularPolygon.java
+++ polygon/IrregularPolygon.java
@@ -9,20 +9,47 @@ public class IrregularPolygon {
 
     // constructor
     public IrregularPolygon() {}
+       public void add(Point2D.Double aPoint)
+    {
+        myPolygon.add(aPoint);
+    }
 
     // public methods
     public void add(Point2D.Double aPoint)
     {
-        // TODO: Add a point to the IrregularPolygon.
+          myPolygon.add(aPoint);
     }
 
     public double perimeter() {
-        // TODO: Calculate the perimeter.
+          public double perimeter() {
+        if (myPolygon.size() < 2) return 0;
+
+        double total = 0;
+
+        for (int i = 0; i < myPolygon.size(); i++) {
+            Point2D.Double p1 = myPolygon.get(i);
+            Point2D.Double p2 = myPolygon.get((i + 1) % myPolygon.size());
+            total += p1.distance(p2);
+        }
+
         return 3.14;
     }
 
     public double area() {
-        // TODO: Calculate the area.
+        public double area() {
+        if (myPolygon.size() < 3) return 0;
+
+        double sum = 0;
+
+        for (int i = 0; i < myPolygon.size(); i++) {
+            Point2D.Double p1 = myPolygon.get(i);
+            Point2D.Double p2 = myPolygon.get((i + 1) % myPolygon.size());
+
+            sum += (p1.x * p2.y) - (p2.x * p1.y);
+        }
+
+        return Math.abs(sum) / 2.0;
+    }
         Double area = 0.0;
         return area;
     }
@@ -31,10 +58,21 @@ public class IrregularPolygon {
     {
         // Wrap the DrawingTool in a try/catch to allow development without need for graphics.
         try {
-            // TODO: Draw the polygon.
-            // Documents: https://pavao.org/compsci/gpdraw/html/gpdraw/DrawingTool.html
-            //DrawingTool myDrawingTool = new DrawingTool(new SketchPad(500, 500));
-            //myDrawingTool.move(50, 50);
+             try {
+            DrawingTool myDrawingTool = new DrawingTool(new SketchPad(500, 500));
+
+            if (myPolygon.size() == 0) return;
+
+            Point2D.Double start = myPolygon.get(0);
+            myDrawingTool.move(start.x, start.y);
+
+            for (int i = 1; i < myPolygon.size(); i++) {
+                Point2D.Double p = myPolygon.get(i);
+                myDrawingTool.move(p.x, p.y);
+            }
+
+            // Close the polygon
+            myDrawingTool.move(start.x, start.y);
         } catch (java.awt.HeadlessException e) {
             System.out.println("Exception: No graphics support available.");
         }
