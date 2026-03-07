b6f52c6442eb68354fc8735366a263bdd3c63663
alexander chang <alexchangsports@gmail.com>
2026-03-07 06:33:33 +0000

    edits late

--- polygon/Main.java
+++ polygon/Main.java
@@ -1,9 +1,16 @@
 package polygon;
 
+import java.awt.geom.Point2D;
+
 public class Main {
     public static void main(String [] args)
     {
         IrregularPolygon myPolygon = new IrregularPolygon();
+          myPolygon.add(new Point2D.Double(50, 50));
+        myPolygon.add(new Point2D.Double(150, 50));
+        myPolygon.add(new Point2D.Double(120, 120));
+        myPolygon.add(new Point2D.Double(60, 150));
+
         myPolygon.draw();
 
         TestSuite.run();
